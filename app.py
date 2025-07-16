505b04038894c4998fdcd05fccba918ef3f4da0c
nishant ali <75194535+nish-bit@users.noreply.github.com>
2025-07-16 09:02:34 +0530

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from langchain.indexes import VectorstoreIndexCreator
 
 # ---------------- GOOGLE LOGIN AUTH -------------------
 
-user = st.experimental_user
+user = st.experimental_get_user()
 ALLOWED_USERS = ["nishant@example.com", "user1@example.com", "test@example.com"]
 ADMIN_USERS = ["nishant@example.com"]
 
